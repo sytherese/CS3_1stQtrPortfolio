9006eaf85f88d9b568d646420e119ad65931db96
sytherese <b2029tatsy@pshs.edu.ph>
2025-08-07 10:27:24 +0800

    1st non graded

---
+++ public/script.js
@@ -0,0 +1,7 @@
+let age = 14;
+let maxAge = 100;
+let numPerDay = 2;
+let totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write("You will need " +totalRequired + " red velvet cupcakes to last you until the ripe old age of " +maxAge );
+
